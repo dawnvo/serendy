2225f4032cefa17fa9530be8b8d328849b8e5773
dawnvo <dawnvora@gmail.com>
2023-06-10 08:42:10 +0900

    feat: MediaQuery 확장

---
+++ lib/src/configs/configs.dart
@@ -0,0 +1,10 @@
+// Constants
+export 'constants/size_constant.dart';
+export 'constants/asset_constant.dart';
+
+// Extensions
+export 'extensions/context_extension.dart';
+
+// Others
+export 'router/app_router.dart';
+export 'theme/app_theme.dart';
--- lib/src/configs/extensions/context_extension.dart
+++ lib/src/configs/extensions/context_extension.dart
@@ -5,4 +5,9 @@ extension ContextX on BuildContext {
   ThemeData get theme => Theme.of(this);
   TextTheme get textTheme => theme.textTheme;
   ColorScheme get colorScheme => theme.colorScheme;
+
+  /// [MediaQueryData]에서 필요없는 속성은 캡슐화해요.
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
+  double get screenWidth => mediaQuery.size.width;
+  double get screenHeight => mediaQuery.size.height;
 }
