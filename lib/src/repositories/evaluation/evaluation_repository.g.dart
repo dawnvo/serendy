29d3d31276cc05300f43285ed0c3ba12a1468c78
dawnvo <dawnvora@gmail.com>
2023-06-13 22:31:09 +0900

    feat: Evaluation repository 추가

---
+++ lib/src/repositories/evaluation/evaluation_repository.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'evaluation_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$evaluationRepositoryHash() =>
+    r'68806f5a62bc83650c67ba8d34dd2764090dff4c';
+
+/// See also [evaluationRepository].
+@ProviderFor(evaluationRepository)
+final evaluationRepositoryProvider =
+    AutoDisposeProvider<EvaluationRepository>.internal(
+  evaluationRepository,
+  name: r'evaluationRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$evaluationRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef EvaluationRepositoryRef = AutoDisposeProviderRef<EvaluationRepository>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
