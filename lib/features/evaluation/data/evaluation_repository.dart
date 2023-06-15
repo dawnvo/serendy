778f4258b6c65e39fe64765b55d84476b854a64a
dawnvo <dawnvora@gmail.com>
2023-06-15 14:09:35 +0900

    fix: Evaluation repository 추상화

--- lib/features/evaluation/data/evaluation_repository.dart
+++ lib/features/evaluation/data/evaluation_repository.dart
@@ -1,34 +1,17 @@
-import 'package:serendy/_mock.dart';
 import 'package:serendy/features/evaluation/domain/evaluation.dart';
 
-final class EvaluationRepository {
-  final List<Evaluation?> _evaluations = [evaluationMock];
+abstract class EvaluationRepository {
+  Future<List<Evaluation?>> fetchEvaluationList();
 
-  Future<List<Evaluation?>> fetchEvaluationList() async {
-    return Future.value(_evaluations);
-  }
+  Stream<List<Evaluation?>> watchEvaluationList();
 
-  Stream<List<Evaluation?>> watchEvaluationList() async* {
-    yield _evaluations;
-  }
+  Future<Evaluation?> fetchEvaluation(String id);
 
-  Future<Evaluation?> fetchEvaluation(String id) async {
-    return Future.value(_evaluations[0]);
-  }
+  Stream<Evaluation?> watchEvaluation(String id);
 
-  Stream<Evaluation?> watchEvaluation(String id) {
-    return watchEvaluationList().map((evaluation) => evaluation[0]);
-  }
+  Future<void> createEvaluation(Evaluation evaluation);
 
-  Future<void> createEvaluation(String id, String imageUrl) {
-    throw UnimplementedError();
-  }
+  Future<void> updateEvaluation(Evaluation evaluation);
 
-  Future<void> updateEvaluation(Evaluation evalEvaluation) {
-    throw UnimplementedError();
-  }
-
-  Future<void> deleteEvaluation(String id) {
-    throw UnimplementedError();
-  }
+  Future<void> removeEvaluation(Evaluation evaluation);
 }
---
+++ lib/features/evaluation/data/evaluation_repository_fake.dart
@@ -0,0 +1,45 @@
+import 'package:serendy/_mock.dart';
+import 'package:serendy/features/evaluation/data/evaluation_repository.dart';
+import 'package:serendy/features/evaluation/domain/evaluation.dart';
+
+final class EvaluationRepositoryFake extends EvaluationRepository {
+  final List<Evaluation?> _evaluations = [evaluationMock];
+
+  @override
+  Future<List<Evaluation?>> fetchEvaluationList() async {
+    return Future.value(_evaluations);
+  }
+
+  @override
+  Stream<List<Evaluation?>> watchEvaluationList() async* {
+    yield _evaluations;
+  }
+
+  @override
+  Future<Evaluation?> fetchEvaluation(String id) async {
+    return Future.value(_evaluations[0]);
+  }
+
+  @override
+  Stream<Evaluation?> watchEvaluation(String id) {
+    return watchEvaluationList().map((evaluation) => evaluation[0]);
+  }
+
+  @override
+  Future<void> createEvaluation(Evaluation evaluation) async {
+    _evaluations.add(evaluation);
+  }
+
+  @override
+  Future<void> updateEvaluation(Evaluation evaluation) async {
+    final index = _evaluations.indexWhere((e) => e?.id == evaluation.id);
+    if (index == -1) return;
+
+    _evaluations[index] = evaluation;
+  }
+
+  @override
+  Future<void> removeEvaluation(Evaluation evaluation) async {
+    _evaluations.removeWhere((e) => e?.id == evaluation.id);
+  }
+}
