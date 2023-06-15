774335480a8c71fb8290c546baa0947b7a36a3d5
dawnvo <dawnvora@gmail.com>
2023-06-15 14:08:58 +0900

    style: mock image url 단축 - 데이터를 관찰할 때 편리해요

--- lib/_mock.dart
+++ lib/_mock.dart
@@ -9,11 +9,10 @@ final userMock = User(name: "Serendy");
 
 // Media
 final mediaMock = Media(
-  image:
-      "https://i.pinimg.com/236x/2b/63/b4/2b63b4c741ffc742f42f321b5f4bd1ec.jpg",
-  title: "최애의 아이",
   type: MediaType.anime,
   status: MediaStatus.finished,
+  title: "최애의 아이",
+  image: "https://vo.la/oRm94",
   keywords: const ["코미디", "액션", "능력"],
 );
 
@@ -43,27 +42,23 @@ final collectionsMock = [
       owner: collectionOwnerMock,
       title: "M3",
       description: "특히 과학연구의 분야에서 실험 도중에 실패해서 얻은 결과에서 중대한 발견 또는 발명을 하는 것을 말한다",
-      image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6XwczmKGbetQaI2AqVBagWKSax0R4U-jdrQ&usqp=CAU",
+      image: "https://vo.la/13rVl",
       items: List.filled(8, collectionItemMock)),
   Collection(
       private: false,
       owner: collectionOwnerMock,
       title: "자바스크립트",
-      image:
-          "https://velog.velcdn.com/images/sehyunny/post/88174e81-911d-4120-baef-08fc8a581bde/image.png",
+      image: "https://vo.la/hOQob",
       items: List.filled(8, collectionItemMock)),
   Collection(
     private: false,
     owner: collectionOwnerMock,
     title: "ES2023에서 도입된 자바스크립트의 새로운 배열 복사 메서드",
-    image:
-        "https://velog.velcdn.com/images/eunbinn/post/1db97b15-37a6-4cda-8bae-f735c41166c7/image.webp",
+    image: "https://vo.la/M5xSg",
   ),
   Collection(
     owner: collectionOwnerMock,
     title: "소 잃고 뇌약간 고친 MSA 구현기",
-    image:
-        "https://velog.velcdn.com/images/ingkoon/post/96140f7b-094b-49ac-8043-9cbc3d1613ee/image.png",
+    image: "https://vo.la/Zzy1u",
   ),
 ];
