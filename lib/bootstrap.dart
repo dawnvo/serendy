3bae3535d1e25de29de65f70113aa9a4c911af3e
dawnvo <dawnvora@gmail.com>
2023-06-21 05:53:27 +0900

    feat: firebase 초기화

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,17 +2,27 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:serendy/bloc_observer.dart';
-import 'package:serendy/configs/di/locator.dart';
+import 'package:serendy/core/locator.dart';
+import 'package:serendy/firebase_options.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // BloC
   Bloc.observer = const AppBlocObserver();
 
-  await setupServiceLocator();
+  // Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // GetIt dependecies
+  AppBinding.dependencies();
+
   runApp(await builder());
 }
